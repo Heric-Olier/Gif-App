2cefc884e1aae1870524829b86107ee1eaebe7e5
Heric Olier <hericolier.code@gmail.com>
2022-10-26 16:08:52 -0500

    Validate Max Amount Gif to Show

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,23 @@
 import { GifGridItem } from "./GifGridItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { SeeMore } from "./SeeMore";
+import { Alerts } from "./Alerts";
+
 
 export const GifGrid = ({ categories }) => {
   const { images, isLoading, page, setPage } = useFetchGifs(categories, 20);
-
+  console.log(images.length, "images.length");
+  
   const handleSeeMore = () => {
+   if (images.length >= 50) {
+    Alerts.fire({
+      icon: "error",
+      title: "No more gifs to show",
+    });
+    return
+   } else {
     setPage(page + 20);
+   }
   };
 
   return (
@@ -24,9 +35,9 @@ export const GifGrid = ({ categories }) => {
               />
             );
           })}
+      <SeeMore onClick={handleSeeMore} />
         </div>
       )}
-      <SeeMore onClick={handleSeeMore} />
     </>
   );
 }

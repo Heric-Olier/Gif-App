0fa2d9acfbda8a5153baf9ccd22218540371f817
Heric Olier <hericolier.code@gmail.com>
2022-10-26 08:25:49 -0500

    Add Open Link Img Function

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -1,7 +1,14 @@
 
-export const GifGridItem = ({ titile, url, id }) => {
+export const GifGridItem = ({ titile, url, link }) => {
+  
+  const handleOpenLink = () => {
+    window.open(link, "_blank"); // abre el link en una nueva pestaña
+  };
+
+
+
   return (
-    <div className="card">
+    <div className="card" onClick={handleOpenLink}>
         <img src={url} alt={titile} />
         <p>{titile.charAt(0).toUpperCase() + titile.slice(1)}</p> {/* mostramos el titulo de la imagen */}
     </div>
